7f722506aa931cd9443550d128b1aea09d06ad4f
Norman Jäckel <lepus@normanjaeckel.de>
2022-08-30 23:31:24 +0200

    Added next test.

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -85,6 +85,42 @@ suite =
                             ]
 
             -- Alternative mit alter Datenstruktur:  [ Assignment.Model e5 [ p10, p5 ], Assignment.Model e4 [ p7, p9 ], Assignment.Model e3 [ p8, p6 ], Assignment.Model e1 [ p1, p4 ], Assignment.Model e2 [ p2, p3 ]]
+            , test "assigns all pupils with changed choices" <|
+                \_ ->
+                    let
+                        pupils =
+                            [ p1, p2, p3, p4, p5, p6, p7, p8, p9, { p10 | choices = [ Pupil.Choice e5 Pupil.Red ] } ]
+                    in
+                    Main.finalize pupils [ e1, e2, e3, e4, e5 ]
+                        |> Expect.all
+                            [ Dict.toList
+                                >> Expect.equalLists
+                                    [ ( "Ali (1a)", "Theater-1" )
+                                    , ( "Anna (1a)", "Song des Tages-1" )
+                                    , ( "Hans (1a)", "Outdoor-2" )
+                                    , ( "Josua (1a)", "Outdoor-1" )
+                                    , ( "Kim (1a)", "Töpfern-1" )
+                                    , ( "Lisa (1a)", "Kochen-1" )
+                                    , ( "Maria (1a)", "Theater-2" )
+                                    , ( "Max (1a)", "Töpfern-2" )
+                                    , ( "Moritz (1a)", "Kochen-2" )
+                                    , ( "Richard (1a)", "Song des Tages-2" )
+                                    ]
+
+                            -- [ ( "Ali (1a)", "Theater-1" )
+                            -- , ( "Anna (1a)", "Outdoor-1" )
+                            -- , ( "Hans (1a)", "Outdoor-2" )
+                            -- , ( "Josua (1a)", "Song des Tages-1" )
+                            -- , ( "Kim (1a)", "Töpfern-1" )
+                            -- , ( "Lisa (1a)", "Kochen-1" )
+                            -- , ( "Maria (1a)", "Theater-2" )
+                            -- , ( "Max (1a)", "Töpfern-2" )
+                            -- , ( "Moritz (1a)", "Kochen-2" )
+                            -- , ( "Richard (1a)", "Song des Tages-2" )
+                            -- ]
+                            , Dict.keys >> List.length >> Expect.equal 10
+                            , howManyGreens pupils >> Expect.equal 7
+                            ]
             ]
         ]
 
